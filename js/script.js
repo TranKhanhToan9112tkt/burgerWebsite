5c6fe9b85150aa1b9775190cb6eb9776fd44e945
Trần Khánh Toàn <131358059+TranKhanhToan9112tkt@users.noreply.github.com>
2023-05-20 20:11:10 +0700

    Update script.js

---
+++ js/script.js
@@ -0,0 +1,25 @@
+const searchForm = document.querySelector('.search-form')
+const searchBtn = document.querySelector('.search-btn')
+const menuBtn = document.querySelector('.menu-btn')
+const navBar = document.querySelector('.navbar')
+
+searchBtn.onclick = function(){
+    searchBtn.classList.toggle('fa-times')
+    searchForm.classList.toggle('active');
+    menuBtn.classList.remove('fa-times');
+    navBar.classList.remove('active');
+}
+
+menuBtn.onclick = function(){
+    menuBtn.classList.toggle('fa-times')
+    searchBtn.classList.remove('fa-times')
+    searchForm.classList.remove('active');
+    navBar.classList.toggle('active');
+}
+
+window.onscroll = function(){
+    searchForm.classList.remove('active');
+    searchBtn.classList.remove('fa-times');
+    menuBtn.classList.remove('fa-times');
+    navBar.classList.remove('active');
+}
