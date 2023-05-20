c415309ba60f3bae16ea05270f3415c8e583ce68
Trần Khánh Toàn <131358059+TranKhanhToan9112tkt@users.noreply.github.com>
2023-05-20 20:19:27 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,18 +1,18 @@
-const searchForm = document.querySelector('.search-form')
-const searchBtn = document.querySelector('.search-btn')
-const menuBtn = document.querySelector('.menu-btn')
-const navBar = document.querySelector('.navbar')
+const searchForm = document.querySelector('.search-form');
+const searchBtn = document.querySelector('.search-btn');
+const menuBtn = document.querySelector('.menu-btn');
+const navBar = document.querySelector('.navbar');
 
 searchBtn.onclick = function(){
-    searchBtn.classList.toggle('fa-times')
+    searchBtn.classList.toggle('fa-times');
     searchForm.classList.toggle('active');
     menuBtn.classList.remove('fa-times');
     navBar.classList.remove('active');
 }
 
 menuBtn.onclick = function(){
-    menuBtn.classList.toggle('fa-times')
-    searchBtn.classList.remove('fa-times')
+    menuBtn.classList.toggle('fa-times');
+    searchBtn.classList.remove('fa-times');
     searchForm.classList.remove('active');
     navBar.classList.toggle('active');
 }
